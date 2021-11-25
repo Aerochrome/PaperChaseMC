7de556542e80723654a8e52f0da6e48bc5cff3a4
Philipp Lätsch <p.laetsch@outlook.de>
2021-11-25 21:45:07 +0100

    Initial Commit

---
+++ src/main/java/de/kappa/paperchase/Main.java
@@ -0,0 +1,13 @@
+package de.kappa.paperchase;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin
+{
+
+    @Override
+    public void onEnable() {
+        Bukkit.getLogger().info("Starting");
+    }
+}
