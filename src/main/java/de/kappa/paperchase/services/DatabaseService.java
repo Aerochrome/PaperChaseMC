060bb6c0feb9845da6a9ea4c186a8ca6c8df6aed
Philipp Lätsch <p.laetsch@outlook.de>
2021-11-27 23:47:24 +0100

    Scaffold DatabaseService

---
+++ src/main/java/de/kappa/paperchase/services/DatabaseService.java
@@ -0,0 +1,14 @@
+package de.kappa.paperchase.services;
+
+public class DatabaseService {
+
+    public void connectByConfiguration() {
+        // Get config values
+        // TODO
+    }
+
+    public void connect() {
+        // TODO
+    }
+
+}
