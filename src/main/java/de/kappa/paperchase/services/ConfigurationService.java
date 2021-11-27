7ff61199115635315dcb25c3946f45fcb8d1b659
Philipp Lätsch <p.laetsch@outlook.de>
2021-11-28 00:40:44 +0100

    Added database connection method
    -
    - DatabaseService now provides a method to get a database connection based on config values

--- src/main/java/de/kappa/paperchase/services/ConfigurationService.java
+++ src/main/java/de/kappa/paperchase/services/ConfigurationService.java
@@ -4,6 +4,8 @@ import de.kappa.paperchase.Main;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.Plugin;
 
+import java.util.HashMap;
+
 public class ConfigurationService {
 
     public static FileConfiguration config;
@@ -15,4 +17,17 @@ public class ConfigurationService {
     public void loadConfig() {
         ConfigurationService.config = Main.instance.getConfig();
     }
+
+    public static HashMap<String, String> getDatabaseConfigValues() {
+        HashMap<String, String> dbConfigValues = new HashMap<String, String>();
+
+        dbConfigValues.put("host", config.getString("db_host", "localhost"));
+        dbConfigValues.put("port", config.getString("db_port", "3306"));
+        dbConfigValues.put("user", config.getString("db_user", "root"));
+        dbConfigValues.put("password", config.getString("db_password", "root"));
+        dbConfigValues.put("database", config.getString("db_database", "paperchasemc"));
+
+        return dbConfigValues;
+    }
+
 }
--- src/main/java/de/kappa/paperchase/services/DatabaseService.java
+++ src/main/java/de/kappa/paperchase/services/DatabaseService.java
@@ -1,14 +1,27 @@
 package de.kappa.paperchase.services;
 
+import de.kappa.paperchase.Main;
+
+import java.sql.*;
+import java.util.HashMap;
+
 public class DatabaseService {
 
-    public void connectByConfiguration() {
-        // Get config values
-        // TODO
-    }
+    public static Connection getConnection() {
+        // TODO implement connection pooling with HikariCP in the future to prevent overhead
 
-    public void connect() {
-        // TODO
-    }
+        HashMap<String, String> dbConfigValues = ConfigurationService.getDatabaseConfigValues();
 
+        String jdbcUrl = "jdbc:mysql://" + dbConfigValues.get("host") + ":" + dbConfigValues.get("port") + "/" +
+                dbConfigValues.get("database");
+
+        try {
+            return DriverManager
+                    .getConnection(jdbcUrl, dbConfigValues.get("user"), dbConfigValues.get("password"));
+        } catch (SQLException e) {
+            Main.instance.getLogger().severe("Could not establish database connection");
+        }
+
+        return null;
+    }
 }
