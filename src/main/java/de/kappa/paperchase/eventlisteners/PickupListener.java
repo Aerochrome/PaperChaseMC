c9398f9cd259ceb6b9964faccb9c92ba7aedfa12
Philipp Lätsch <p.laetsch@outlook.de>
2021-11-28 19:16:25 +0100

    added cooldown to pickupevent for paperchase items

--- src/main/java/de/kappa/paperchase/eventlisteners/PickupListener.java
+++ src/main/java/de/kappa/paperchase/eventlisteners/PickupListener.java
@@ -8,22 +8,42 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityPickupItemEvent;
+import org.bukkit.inventory.ItemStack;
+import java.util.HashMap;
 
 public class PickupListener implements Listener {
+    public HashMap<String, Long> pickupCooldownMap = new HashMap<String, Long>();
+
     @EventHandler
     public void onItemPickup(EntityPickupItemEvent event) {
-        // Cancel pickup
-        Material material = event.getItem().getItemStack().getType();
+        if (event.getEntityType() != EntityType.PLAYER) {
+            event.setCancelled(true);
+            return;
+        }
+
+        ItemStack stack = event.getItem().getItemStack();
+        Material material = stack.getType();
 
         if (material == Material.COOKIE) {
 
-            Integer paperChaseId;
-            if ((paperChaseId = ItemLibrary.getPaperChaseId(event.getItem().getItemStack())) != null) {
-                if (event.getEntityType() == EntityType.PLAYER) {
-                    Player p = (Player) event.getEntity();
-                    p.sendMessage(ChatColor.RED + "[PaperMC] Found cookie #" + paperChaseId.toString());
-                }
+            // Cooldown check
+            String itemUUIDString = event.getItem().getUniqueId().toString();
 
+            Long cooldownUntil = this.pickupCooldownMap.get(itemUUIDString);
+            if (cooldownUntil != null) {
+                if (System.currentTimeMillis()/1000L <= cooldownUntil) {
+                    event.setCancelled(true);
+                    return;
+                }
+            }
+
+            Player p = (Player) event.getEntity();
+
+            Integer paperChaseId;
+            if ((paperChaseId = ItemLibrary.getPaperChaseId(stack)) != null) {
+                p.sendMessage(ChatColor.RED + "[PaperMC] Found cookie #" + paperChaseId.toString());
+
+                this.pickupCooldownMap.put(itemUUIDString, System.currentTimeMillis()/1000L+2);
                 event.setCancelled(true);
             }
         }
